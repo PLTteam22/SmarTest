6b4947dbf4dd2895d8bdb5b2f6ec7c8491211cb1
Aiman Najjar <aiman@dyn-209-2-226-127.dyn.columbia.edu>
2012-04-10 01:57:40 -0400

    changed ASTNode constructor to accept yyline, yycolumn

--- src/ASTNode.java
+++ src/ASTNode.java
@@ -9,10 +9,15 @@ public class ASTNode {
 	
 	private ArrayList<ASTNode> children;
 	private String type;	
-
-	public ASTNode()
+	private int yyline;
+	private int yycolumn;
+	
+	
+	public ASTNode(int yyline, int yycolumn)
 	{
 		this.children = new ArrayList<ASTNode>();
+		this.yyline = yyline;
+		this.yycolumn = yycolumn;
 	}
 	
 	ASTNode getChildAt(int index)
@@ -31,6 +36,22 @@ public class ASTNode {
 
 	void setType(String typ){
 		this.type= typ;
+	}
+
+	public int getYyline() {
+		return yyline;
+	}
+
+	public void setYyline(int yyline) {
+		this.yyline = yyline;
+	}
+
+	public int getYycolumn() {
+		return yycolumn;
+	}
+
+	public void setYycolumn(int yycolumn) {
+		this.yycolumn = yycolumn;
 	}
 
 
--- src/stl.y
+++ src/stl.y
@@ -1,7 +1,6 @@
 %{
 
 import java.io.*;
-import smartest.*;
 
 %}
 
