0b563fbb4b22d010c53f141220c388e8a9a51709
Dan Walker <dan.walker1@gmail.com>
2012-04-10 19:03:37 -0400

    Added class FunctionSymbolTableEntry to encapsulate function symbol info for the symbol table

---
+++ src/FunctionSymbolTableEntry.java
@@ -0,0 +1,23 @@
+public class FunctionSymbolTableEntry
+{
+        private String stlID, javaID;
+        private ArrayList<String> paramTypes;
+        public FunctionSymbolTableEntry(String id, String javaID, ArrayList<String> parameterTypes)
+        {
+                stlID = id;
+                this.javaID = javaID;
+                paramTypes = parameterTypes;
+        }
+        public String getID()
+        {
+                return stlID;
+        }
+        public String getJavaID()
+        {
+                return javaID;
+        }
+        public ArrayList<String> getParamTypes()
+        {
+                return paramTypes;
+        }
+}
