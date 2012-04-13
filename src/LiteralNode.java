a49782c880056997549f46129f7e8b275c00c5e8
Harpreet Singh <harpreet2301@yahoo.com>
2012-04-13 17:52:09 -0400

    setting values

--- src/LiteralNode.java
+++ src/LiteralNode.java
@@ -1,9 +1,36 @@
 
 public class LiteralNode extends ASTNode {
 
-	public LiteralNode(String type, int yyline, int yycolumn) {
+	public LiteralNode(String type, Object object, int yyline, int yycolumn) 
+	{
 		super(yyline, yycolumn);
 		this.setType(type);
+		if(type.equalsIgnoreCase("boolean"))
+		{
+			boolean value=(boolean)object;
+			this.setBvalue(value);
+		}
+		else if(type.equalsIgnoreCase("int"))
+		{
+			int value=(int)object;
+			this.setIvalue(value);
+		}
+		else if(type.equalsIgnoreCase("float"))
+		{
+			float value=(float)object;
+			this.setFvalue(value);
+		}
+		else if(type.equalsIgnoreCase("string"))
+		{
+			String value=(String)object;
+			this.setSvalue(value);
+		}
+		else
+		{
+			int value =(int)object;
+			char c=(char)value;
+			this.setCvalue(c);
+		}
 	}
 
 	@Override
