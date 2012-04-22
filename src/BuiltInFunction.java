34ca100bd49c98770753c9e2b57d5dff7ddf23fd
Parth  solnki <ps2678@columbia.edu>
2012-04-21 22:36:37 -0400

    updating ask questions, WIP..

--- src/BuiltInFunction.java
+++ src/BuiltInFunction.java
@@ -1,92 +1,123 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.Iterator;
 import java.util.Random;
 
-
 public class BuiltInFunction {
-	
-	//load method
-		StlSetNode load(String connection_string, String category){
-			
-			return null;
-		}
-	
-	//askQuestion method
-		int askQuestion(StlSetNode s){
-			boolean isFull = false;
-			float points = 0;
-			while(true){	
-			
-				int size = s.getQuestionArrayList().size();
-				//generate random number for picking question from set randomly
-				int item = new Random().nextInt(size);
-								
-				if(size>0){
-				
-					//check if all questions have been asked once
-					if(size == s.getQuestionIsAskedHM().size()){
-						isFull = true;
-					}
+
+	// load method
+	StlSetNode load(String connection_string, String category) {
+
+		return null;
+	}
+
+	// askQuestion method
+	int askQuestion(StlSetNode s) {
+		boolean isFull = false;
+		int points = 0;
+		boolean isDone = false;
+		while (!isDone) {
+
+			int size = s.getQuestionArrayList().size();
+			// generate random number for picking question from set randomly
+			int item = new Random().nextInt(size);
+
+			if (size > 0) {
+
+				// check if all questions have been asked once
+				if (size == s.getQuestionIsAskedHM().size()) {
+					isFull = true;
+				}
+
+				for (int i = 0; i < s.getQuestionArrayList().size(); i++) {
+					if (i == item) {
+						// if all questions have been asked once pick randomly
+						if (isFull) {
 							
-					for(int i=0;i<s.getQuestionArrayList().size();i++)
-					{
-					    if (i == item){
-					    	//if all questions have  been asked once pick randomly
-					    	if(isFull){
-					    		//print question
-					    		//wait to read the input from user
-					    		//set the points obtained to points variable
-					    		break;
-					    	}
-					    	else{
-					    		//check if the question is in the hashmap or not and add to the hashmap if not exists
-					    		if(!( s.getQuestionIsAskedHM().containsKey(s.getQuestionArrayList().get(i)))){
-					    			s.getQuestionIsAskedHM().put((QuestionLiteralNode)s.getQuestionArrayList().get(i), true);
-					    			//print question
-						    		//wait to read the input from user
-						    		//set the points obtained to points variable
-					    			break;
-					    		}					    		
-					    		
-					    	}					    	
-					    	
-					    }				       
+							// print question
+							
+							// wait to read the input from user
+							BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+							try {
+								String inputAnswer = reader.readLine();
+							} catch (IOException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+								System.out.println("Invalid Input");
+							}
+							
+							// set the points obtained to points variable
+							//points = Integer.parseInt(s.getQuestionArrayList().get(i).getAnswerChoices().getSvalue());
+							
+							isDone = true;// break;
+						}
+						// check if the question is in the hashmap or not and
+						// add to the hashmap if not exists
+						else if (!(s.getQuestionIsAskedHM().containsKey(s
+								.getQuestionArrayList().get(i)))) {
+							s.getQuestionIsAskedHM().put(
+									(QuestionLiteralNode) s
+											.getQuestionArrayList().get(i),
+									true);
+							
+							// print question
+							
+							// wait to read the input from user
+							BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+							try {
+								String inputAnswer = reader.readLine();
+							} catch (IOException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+								System.out.println("Invalid Input");
+							}
+							
+							// set the points obtained to points variable
+							isDone = true;// break;
+						}
 					}
 				}
-			}	
-			return 0;
+			}
 		}
-	//readLine
-		String readline(){
-			return null;
-		}
-	//print
-		void print(String s){
-			System.out.println(s);
-		}
-	//printVar for int 
-		void printVar(int x){
-			System.out.println(x);
-		}
-					
-	//printVar for char
-		void printVar(char x){
-			System.out.println(x);
-		}
+		return points;
+	}
 
-	//printVar for float
-		void printVar(float x){
-			System.out.println(x);
-		}
-		
-	//printVar for set and questions
-		void printVar(Object x){
-			System.out.println(x);
-		}
-	//length of a set
-		int len (StlSetNode s){
-			return s.getQuestionArrayList().size();
-		}
+	// readLine
+	String readline() {
+		return null;
+	}
+
+	// print
+	void print(String s) {
+		System.out.println(s);
+	}
+
+	// printVar for int
+	void printVar(int x) {
+		System.out.println(x);
+	}
+
+	// printVar for char
+	void printVar(char x) {
+		System.out.println(x);
+	}
+
+	// printVar for float
+	void printVar(float x) {
+		System.out.println(x);
+	}
+
+	// printVar for set and questions
+	void printVar(Object x) {
+		System.out.println(x);
+	}
+
+	// length of a set
+	int len(StlSetNode s) {
+		return s.getQuestionArrayList().size();
+	}
 
 }
--- src/StlSetNode.java
+++ src/StlSetNode.java
@@ -2,37 +2,73 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 
-
+/**
+ * The Class StlSetNode.
+ */
 public class StlSetNode {
 	
-	ArrayList QuestionArrayList = null;
+	/** The Question array list. */
+	ArrayList<QuestionLiteralNode> QuestionArrayList = new ArrayList<QuestionLiteralNode>();
+	
+	
 	//hashmap to store info about a question is asked or not
+	/** The Question is asked hm. */
 	HashMap<QuestionLiteralNode, Boolean> QuestionIsAskedHM = new HashMap<QuestionLiteralNode, Boolean>();
 
 	
-	public ArrayList getQuestionArrayList() {
+	/**
+	 * Gets the question array list.
+	 *
+	 * @return the question array list
+	 */
+	public ArrayList<QuestionLiteralNode> getQuestionArrayList() {
 		return QuestionArrayList;
 	}
 
+	/**
+	 * Sets the question array list.
+	 *
+	 * @param questionArrayList the new question array list
+	 */
 	public void setQuestionArrayList(ArrayList questionArrayList) {
 		QuestionArrayList = questionArrayList;
 	}
 
+	/**
+	 * Gets the question is asked hash map
+	 *
+	 * @return the question is asked hash map
+	 */
 	public HashMap<QuestionLiteralNode, Boolean> getQuestionIsAskedHM() {
 		return QuestionIsAskedHM;
 	}
 
+	/**
+	 * Sets the question is asked hash map.
+	 *
+	 * @param questionIsAskedHM the question is asked hash map
+	 */
 	public void setQuestionIsAskedHM(
 			HashMap<QuestionLiteralNode, Boolean> questionIsAskedHM) {
 		QuestionIsAskedHM = questionIsAskedHM;
 	}
 
-	//add question literal node to set
+	/**
+	 * Adds the question.
+	 * Add question literal node to set
+	 * @param q the question literal node
+	 */
 	void addQuestion(QuestionLiteralNode q){
 		this.getQuestionArrayList().add(q);
 	}
 		
 	//mark if the question is asked from the set or not
+	/**
+	 * Mark is checked true.
+	 *
+	 * @param s the stlset node
+	 * @param q the question literal node
+	 */
 	void markIsCheckedTrue(StlSetNode s, QuestionLiteralNode q){
 		s.getQuestionIsAskedHM().put(q, true);
 		
