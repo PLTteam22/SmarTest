c5302ebc05d9d4f77e9fdf1b2bf6c361cd0bd145
Parth  solnki <ps2678@columbia.edu>
2012-04-22 22:03:57 -0400

    coded askQuestions

--- src/BuiltInFunction.java
+++ src/BuiltInFunction.java
@@ -1,40 +1,49 @@
-
+/*
+ * 
+ */
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.Iterator;
 import java.util.Random;
 
+
+/**
+ * The Class BuiltInFunction.
+ * 
+ * Contains a list of Built In Functions in STL
+ * @author Parth
+ */
 public class BuiltInFunction {
 
 	// load method
-	StlSetNode load(String connection_string, String userName, String password, String category){
-		
-		Connection conn = null;
-		String driver = "com.mysql.jdbc.Driver";
-		try {
-			Class.forName(driver).newInstance();
-			conn = DriverManager.getConnection(connection_string,userName,password);
-			System.out.println("Connected to the database");
-			conn.close();
-			System.out.println("Disconnected from database");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
+	/**
+	 * Load.
+	 *
+	 * @param connection_string the connection_string
+	 * @param category the category
+	 * @return the stl set node
+	 */
+	StlSetNode load(String connection_string, String category) {
 
 		return null;
 	}
 
 	// askQuestion method
+	/**
+	 * Ask question.
+	 *
+	 * @param s the StlSetNode
+	 * @return the int
+	 */
 	int askQuestion(StlSetNode s) {
 		boolean isFull = false;
 		int points = 0;
 		boolean isDone = false;
+		String inputAnswer = null;
 		while (!isDone) {
 
 			int size = s.getQuestionArrayList().size();
@@ -49,25 +58,39 @@ public class BuiltInFunction {
 				}
 
 				for (int i = 0; i < s.getQuestionArrayList().size(); i++) {
+					ArrayList<AnswerChoice> answerChoiceArrayList = s.getQuestionArrayList().get(i)
+							.getAnswers();
+					
 					if (i == item) {
 						// if all questions have been asked once pick randomly
 						if (isFull) {
-							
+
 							// print question
+							System.out.println(s.getQuestionArrayList().get(i).getQuestionText()+"\n");
+							
+							for(int k=0;k<answerChoiceArrayList.size();k++){
+								System.out.println(answerChoiceArrayList.get(k).getText()+
+										":"+answerChoiceArrayList.get(k).getPoints()+"\n");
+							}
 							
 							// wait to read the input from user
-							BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+							BufferedReader reader = new BufferedReader(
+									new InputStreamReader(System.in));
 							try {
-								String inputAnswer = reader.readLine();
+								inputAnswer = reader.readLine();
 							} catch (IOException e) {
 								// TODO Auto-generated catch block
 								e.printStackTrace();
 								System.out.println("Invalid Input");
 							}
-							
+
 							// set the points obtained to points variable
-							//points = Integer.parseInt(s.getQuestionArrayList().get(i).getAnswerChoices().getSvalue());
-							
+							for(int j=0 ; j<answerChoiceArrayList.size() ; j++){
+								if(inputAnswer.equals(answerChoiceArrayList.get(j).getText())){
+									points = answerChoiceArrayList.get(j).getPoints();
+								}
+								
+							}
 							isDone = true;// break;
 						}
 						// check if the question is in the hashmap or not and
@@ -75,23 +98,32 @@ public class BuiltInFunction {
 						else if (!(s.getQuestionIsAskedHM().containsKey(s
 								.getQuestionArrayList().get(i)))) {
 							s.getQuestionIsAskedHM().put(
-									(QuestionLiteralNode) s
+									(Question) s
 											.getQuestionArrayList().get(i),
 									true);
-							
+
 							// print question
 							
+							
 							// wait to read the input from user
-							BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+							BufferedReader reader = new BufferedReader(
+									new InputStreamReader(System.in));
 							try {
-								String inputAnswer = reader.readLine();
+								inputAnswer = reader.readLine();
 							} catch (IOException e) {
 								// TODO Auto-generated catch block
 								e.printStackTrace();
 								System.out.println("Invalid Input");
 							}
-							
+
 							// set the points obtained to points variable
+							for(int j=0 ; j<answerChoiceArrayList.size() ; j++){
+								if(inputAnswer.equals(answerChoiceArrayList.get(j).getText())){
+									points = answerChoiceArrayList.get(j).getPoints();
+								}
+								
+							}
+							
 							isDone = true;// break;
 						}
 					}
@@ -102,36 +134,72 @@ public class BuiltInFunction {
 	}
 
 	// readLine
+	/**
+	 * Readline input
+	 *
+	 * @return the string
+	 */
 	String readline() {
 		return null;
 	}
 
 	// print
+	/**
+	 * Prints the string
+	 *
+	 * @param s the string
+	 */
 	void print(String s) {
 		System.out.println(s);
 	}
 
 	// printVar for int
+	/**
+	 * Prints the int variable
+	 *
+	 * @param x the int
+	 */
 	void printVar(int x) {
 		System.out.println(x);
 	}
 
 	// printVar for char
+	/**
+	 * Prints the char variable
+	 *
+	 * @param x the char
+	 */
 	void printVar(char x) {
 		System.out.println(x);
 	}
 
 	// printVar for float
+	/**
+	 * Prints the float variable
+	 *
+	 * @param x the 
+	 */
 	void printVar(float x) {
 		System.out.println(x);
 	}
 
 	// printVar for set and questions
+	/**
+	 * Prints the set and question variable
+	 *
+	 * @param x the object of type set/question
+	 */
 	void printVar(Object x) {
 		System.out.println(x);
 	}
 
 	// length of a set
+	/**
+	 * Length of set
+	 *
+	 * @param s the StlSetNode
+	 * @return the length of the set as int
+	 */
 	int len(StlSetNode s) {
 		return s.getQuestionArrayList().size();
 	}
